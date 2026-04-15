399a42f7ccf21f7185097730617ce2de67a163f6
Dinh Phanh <bong16122005@gmail.com>
2026-04-15 19:38:43 +0700

    feat: implement Redux state management for authentication and update project-wide gitignore rules

--- backend/scratch.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb+srv://root:123@cluster0.pht5w.mongodb.net/cuuho?retryWrites=true&w=majority';
-mongoose.connect(url).then(async () => {
-  const db = mongoose.connection.db;
-  const teams = await db.collection('rescueteams').find({}, { projection: { name: 1, status: 1, lastLocationUpdate: 1 } }).toArray();
-  console.log("Teams:", teams);
-  process.exit(0);
-});
--- backend/scratch/test_tc12_cluster.js
+++
@@ -1,5 +0,0 @@
-const { spawn } = require('child_process');
-const numClients = 10;
-for (let i = 0; i < numClients; i++) {
-  spawn('node', ['test_tc12_ratelimit.js'], { stdio: 'inherit' });
-}
